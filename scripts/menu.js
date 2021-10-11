a0ed1aca5396430ca77932bbcbce72cc14fdd0aa
Egorov Oleg <88430433+egorovow@users.noreply.github.com>
2021-10-11 11:09:33 +0400

    Add files via upload

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,12 @@
 let fullscreenMenu = document.querySelector (".fullscreen-menu");
 let hamburger = document.querySelector (".hamburger");
+let fullscreenMenuClose = document.querySelector (".fullscreen-menu__close")
 
 function toggleMenu () {
+  event.preventDefault();
   hamburger.classList.toggle ('hamburger--active');
   fullscreenMenu.classList.toggle ('fullscreen-menu--active');
 };
 
 hamburger.addEventListener ('click', toggleMenu);
+fullscreenMenuClose.addEventListener ('click', toggleMenu);
